bbd0e1c970fbd501ef0d2b96187f6216de306a35
spartan172321 <chronger1115@gmail.com>
2017-08-08 09:23:46 -0400

    map is up

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,5 +1,6 @@
 // Include React
 import React, { Component } from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 // import React Router
 import {
@@ -11,20 +12,66 @@ import {
 
 class Maps extends Component {
 
-	constructor() {
-		super()
+	constructor(props) {
+		super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+
+    // binding this to event-handler functions 
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
 	}
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
 
   // Here we render the component
   render() {
 
-    return (
-	<div className="Maps">
-		<h2>This is the component that displays the map.</h2>
-	</div>
+  return (
+    <div className="Maps">
+
+    	<h2>This is the component that displays the map.</h2>
+
+        <Map google={this.props.google}
+         zoom={14}
+         onClick={this.onMapClicked}>
+
+          <Marker onClick={this.onMarkerClick}
+                  name={'Current location'} />
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+              </div>
+          </InfoWindow>
+
+        </Map>
+
+    </div>
 	)
   }
 }
 
 // Export the component back for use in other files
-export default Maps;
+// export default Maps;
+export default GoogleApiWrapper({apiKey: "AIzaSyAXmfoM3KbRgdzfmvJ7xBJOBmYDZOk20R0"})(Maps)
